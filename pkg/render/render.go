dc36099db9b8b596f68d1e8930e4d61a95c845d0
Sibster <sibster@gmail.com>
2022-11-19 11:35:54 +0100

    Inital commit

---
+++ pkg/render/render.go
@@ -0,0 +1,83 @@
+package render
+
+import (
+	"bytes"
+	"html/template"
+	"log"
+
+	"net/http"
+	"path/filepath"
+
+	"github.com/verscheures/bookings/pkg/config"
+	"github.com/verscheures/bookings/pkg/models"
+)
+
+var app *config.AppConfig
+
+// NewTamplates set config for template packages
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
+// renders template using html templates
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+
+	var tc map[string]*template.Template
+	if app.UseCache {
+		// Get template cahce from app.config
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("Could not get template from template cache")
+	}
+
+	buf := new(bytes.Buffer)
+	td = AddDefaultData(td)
+	_ = t.Execute(buf, td)
+
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	//myCache := make(map[string]*template.Template)
+	myCache := map[string]*template.Template{}
+	// get all *.page.tmpl from ./templases
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+	// range through all files with *.page.tmpl
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[name] = ts
+	}
+	return myCache, nil
+
+}
